ef05def0445d0e2193f021a6f1e340794a16d740
Jalay Movaliya <jalaym123@gmail.com>
2023-04-30 14:17:51 +0530

    add: 2656

---
+++ Easy/2656.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximizeSum(vector<int> &nums, int k)
+    {
+        int max = *max_element(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 0; i < k; i++)
+        {
+            ans += (max + i);
+        }
+        return ans;
+    }
+};
